6a247241b345db4f0893da20e665cf65122adb00
Nimesh Srivastava <snimesh412@gmail.com>
2025-07-25 04:40:21 +0530

    Implemented blob reading

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+use anyhow::Context;
 use clap::{Parser, Subcommand};
-use std::env;
+use flate2::read::ZlibDecoder;
+use std::ffi::CStr;
 use std::fs;
+use std::io;
+use std::io::BufReader;
+use std::io::prelude::*;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -12,10 +17,20 @@ struct Args {
 #[derive(Debug, Subcommand)]
 enum Command {
     Init,
+    CatFile {
+        #[clap(short = 'p')]
+        pretty_print: bool,
+
+        object_hash: String,
+    },
 }
 
-fn main() {
-    println!("Logs");
+enum Kind {
+    Blob,
+}
+
+fn main() -> anyhow::Result<()> {
+    println!("Logs from myGit app...");
 
     let args = Args::parse();
 
@@ -27,5 +42,64 @@ fn main() {
             fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
             println!("Git directory initialised");
         }
+        Command::CatFile {
+            pretty_print,
+            object_hash,
+        } => {
+            anyhow::ensure!(pretty_print, "-p argument is required");
+            let f = std::fs::File::open(format!(
+                ".git/objects/{}/{}",
+                &object_hash[..2],
+                &object_hash[2..]
+            ))
+            .context("open in .git/objects")?;
+
+            let z = ZlibDecoder::new(f);
+            let mut z = BufReader::new(z);
+            let mut buf = Vec::new();
+            z.read_until(0, &mut buf)
+                .context("read header from .git/objects")?;
+
+            let header =
+                CStr::from_bytes_with_nul(&buf).expect("there is only 1 null and that too at end");
+            let header = header
+                .to_str()
+                .context(".git/objects file header is not a valid UTF-8")?;
+
+            let Some((kind, size)) = header.split_once(' ') else {
+                anyhow::bail!(
+                    ".git/objects file header did not start with a known type : '{header}'"
+                );
+            };
+
+            let kind = match kind {
+                "blob" => Kind::Blob,
+                _ => anyhow::bail!("cannot print '{kind}'"),
+            };
+
+            let size = size
+                .parse::<usize>()
+                .context(".git/objects file header has invalid size: {size}")?;
+
+            buf.clear();
+            buf.resize(size, 0);
+            z.read_exact(&mut buf[..])
+                .context("read true contents of .git/objects file")?;
+            let n = z
+                .read(&mut [0])
+                .context("validate EOF in .git/objects file")?;
+            anyhow::ensure!(n == 0, ".git/objects file has {n} trailing bytes");
+
+            let stdout = std::io::stdout();
+            let mut stdout = stdout.lock();
+
+            match kind {
+                Kind::Blob => stdout
+                    .write_all(&buf)
+                    .context("write object contents to stdout")?,
+            }
+        }
     }
+
+    Ok(())
 }
